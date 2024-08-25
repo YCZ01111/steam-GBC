6ca962a023e481ed0e09c90946a49bacb788dd57
YCZ01111 <1104822261@qq.com>
2024-08-25 22:41:26 +0800

    链接仓库

---
+++ Install.cmd
@@ -0,0 +1,29 @@
+@echo off
+FOR /F "tokens=2* skip=2" %%a in ('reg query "HKCU\Software\Valve\Steam" /v "SteamPath"') do set SteamPath=%%b
+REM If return value is not an exist path(Err Msg)
+if not exist "%SteamPath%" (
+    echo No steam detected. Please run steam at least once.
+    pause
+    exit
+)
+for %%I in (.) do set CurDirName=%%~nxI
+if exist "%SteamPath%\steamui\skins\%CurDirName%" (
+    goto Override
+) else (
+    goto Apply
+)
+exit
+
+:Override
+set /P Override=Skin already existed, Override?[y/N]
+if "%Override%"=="y" goto Apply 
+if "%Override%"=="Y" goto Apply
+exit
+
+:Apply
+del /f /q "%SteamPath%\steamui\skins\%CurDirName%"
+mkdir "%SteamPath%\steamui\skins"
+xcopy /e /y "..\%CurDirName%\" "%SteamPath%\steamui\skins\%CurDirName%\"
+del /f /q "%SteamPath%\steamui\skins\%CurDirName%\Install.cmd"
+echo Enjoy your new steam! :^)
+PAUSE
